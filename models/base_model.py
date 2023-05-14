d50d815e5163205cd4abb97080ab63342c01e286
Emmanuel Diogu <emmanueldiogu@gmail.com>
2023-05-14 09:18:36 -0400

    edit docstring

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+"""
+Base Model Class for other model related classes to use.
+"""
+
 from datetime import datetime
 import uuid
-
-from models import storage
+# from models import storage
 
 
 class BaseModel:
-    """Base Model Class
-    Base Model Class for other model related classes to use.
-    """
+    """Base Model Class"""
 
     def __init__(self, *args, **kwargs):
         """
@@ -28,30 +29,25 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            # storage.new(self)
 
     def __str__(self):
-        """string representation
-
-        Returns:
-            str: String representation of
+        """
+        Return string representation of object
             [<class name>] (self.id) <self.__dict__>
         """
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Save update
-        Saves the updated attribute with current datetime
+        """
+        Save the object with current datetime
         """
         self.updated_at = datetime.now()
-        storage.save()
+        # storage.save()
 
     def to_dict(self):
-        """Returns a dictionary
-
-        Returns:
-            dict: returns a dictionary containing all
-            keys/values of __dict__ of the instance
+        """
+        Return dictionary representation of object
         """
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = type(self).__name__
