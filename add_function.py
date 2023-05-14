5229395f70618fe0e06f07f186efc1a7cf45c7be
Emmanuel Diogu <emmanueldiogu@gmail.com>
2023-05-13 20:49:55 -0400

    base model

---
+++ add_function.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+# Example function
+def add_numbers(a, b):
+    return a + b
---
+++ models/base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Base model"""
+
+from datetime import datetime
+import uuid
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = type(self).__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        
+        return new_dict
