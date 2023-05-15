a5c5a10c5e44448ddb355d35b8bafc436a0db84a
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2023-05-14 17:45:26 -0700

    6. Console 0.0.1

--- console.py
+++ console.py
@@ -4,23 +4,18 @@ class HBNBCommand(cmd.Cmd):
      prompt = '(hbnb) '
 
       def do_quit(self, arg):
-           """Exit the program"""
            return True
 
     def do_EOF(self, arg):
-         """Exit the program"""
          return True
 
      def emptyline(self):
-         """Display help message for quit command"""
          print("Exit the program")
 
     def help_EOF(self):
-        """Display help message for EOF command"""
         print("Exit the program")
 
     def help_help(self):
-        """Display help message for help command"""
         print("Display available commands")
 
 if __name__ == '__main__':
